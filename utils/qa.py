0dc2d695df4e30b728666d0f45b9659aba91f650
NARESH KUMAR LAHAJAL <naresh.is21@bmsce.ac.in>
2024-04-26 18:03:32 +0530

    Update qa.py

--- utils/qa.py
+++ utils/qa.py
@@ -5,7 +5,7 @@ from langchain.prompts import PromptTemplate
 from langchain_community.vectorstores import Qdrant
 from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
 from qdrant_client import QdrantClient
-from langchain_community.chat_models import ChatOllama
+#from langchain_community.chat_models import ChatOllama
 
 
 #import chainlit as cl
